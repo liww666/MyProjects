af1971b164a7bfbf968b23f9cfaa76ce53c8af9f
liweiwu <1065034454@qq.com>
2019-01-13 21:50:29 +0800

    consumer 测试提交

--- dubbo-consumer/src/main/java/com/sunyard/dubboconsumer/service/OrderService.java
+++ dubbo-consumer/src/main/java/com/sunyard/dubboconsumer/service/OrderService.java
@@ -4,5 +4,8 @@ package com.sunyard.dubboconsumer.service;
  * Created by Administrator on 2019/1/13.
  */
 public interface OrderService {
+    /*
+    初始化订单
+     */
     String initOrder(String id);
 }
